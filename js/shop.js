94fc7f2a6ad1af7f179baa86231e6a5c60c277f9
0xNordian <85548493+0xNordian@users.noreply.github.com>
2023-05-15 19:45:54 +0200

    Until Exercise 6 completed

--- js/shop.js
+++ js/shop.js
@@ -1,20 +1,17 @@
-//import products from '../products.json';
- 
-var products;
-let JSON_URL = ''
+let products;
 
-// Fetch the JSON data using an HTTP request
-fetch('')
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(data) {
+async function fetchProducts() {
+  try {
+    const response = await fetch('../products.json');
+    const data = await response.json();
     products = data;
-    // Call any functions that depend on the products data
-  })
-  .catch(function(error) {
+
+  } catch (error) {
     console.error('Error fetching products:', error);
-  });
+  }
+}
+
+fetchProducts();
 
  // Array with products (objects) added directly with push(). Products in this array are repeated.
 var cartList = [];
@@ -26,25 +23,53 @@ var total = 0;
 
 // Exercise 1
 function buy(id) {
-    console.log(`TEST + ${products[id]}`);
-    // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cartList array
+  // 1. Loop for to the array products to get the item to add to cart
+  // 2. Add found product to the cartList array
+  let res = JSON.stringify(products[id - 1]);
+  let product = JSON.parse(res);
+  products.forEach((item) => item.id === product.id ? cartList.push(item) : undefined);
+  console.log('Updated cartList:', cartList);
+  calculateTotal();
+  generateCart();
 }
 
 // Exercise 2
 function cleanCart() {
-
+  //console.log(`cartList pre clean: ${JSON.stringify(cartList)}`)
+  cart = [];
+  cartList = [];
+  printCart();
+  //console.log(`cartList post clean: ${JSON.stringify(cartList)}`)
 }
 
 // Exercise 3
 function calculateTotal() {
     // Calculate total price of the cart using the "cartList" array
+    let totalPrice = 0;
+    cartList.forEach((item) => totalPrice += item.price);
+    console.log(`cartList total price: ${totalPrice}`)
+    return totalPrice;
 }
 
 // Exercise 4
 function generateCart() {
     // Using the "cartlist" array that contains all the items in the shopping cart, 
     // generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
+  cart = [];
+  cartList.forEach((item) => {
+    let name = item.name;
+    let existingItemIndex = cart.findIndex((cartItem) => cartItem.name === name);
+    //console.log('existingItemIndex:', existingItemIndex);
+    if (existingItemIndex !== -1) {
+      cart[existingItemIndex].qty += 1;
+      cart[existingItemIndex].total += item.price;
+    } else {
+      cart.push({ name: name, price: item.price, qty: 1, total: item.price });
+    }
+  });
+
+  console.log('Cart:', cart); 
+  printCart();
 }
 
 // Exercise 5
@@ -54,7 +79,39 @@ function applyPromotionsCart() {
 
 // Exercise 6
 function printCart() {
-    // Fill the shopping cart modal manipulating the shopping cart dom
+  const cartListTableBody = document.getElementById('cart_list');
+  cartListTableBody.innerHTML = '';
+
+  const cartItems = {};
+
+  cart.forEach((item) => {
+      const name = item.name;
+      if (!cartItems[name]) {
+          cartItems[name] = { name, qty: item.qty, price: item.price, total: item.total };
+      } else {
+          cartItems[name].qty += item.qty;
+      }
+  });
+
+  Object.values(cartItems).forEach((item) => {
+      const newRow = document.createElement('tr');
+      const nameCell = document.createElement('th');
+      const priceCell = document.createElement('td');
+      const qtyCell = document.createElement('td');
+      const totalCell = document.createElement('td');
+
+      nameCell.textContent = item.name;
+      priceCell.textContent = item.price;
+      qtyCell.textContent = item.qty;
+      totalCell.textContent = item.total;
+
+      newRow.appendChild(nameCell);
+      newRow.appendChild(priceCell);
+      newRow.appendChild(qtyCell);
+      newRow.appendChild(totalCell);
+
+      cartListTableBody.appendChild(newRow);
+  });
 }
 
 
