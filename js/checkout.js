cdb678e8dea40fa8ae52ae623e629c6757f55398
0xNordian <85548493+0xNordian@users.noreply.github.com>
2023-05-30 00:48:35 +0200

    checkout labels completed

--- js/checkout.js
+++ js/checkout.js
@@ -116,15 +116,15 @@ function validate() {
     // First Name validation
     if (fName.value.trim() === "") {
         error++;
-        errorName.textContent = "This field is required and must have, at least, 3 characters";
+        errorName.textContent = "Este campo es obligatorio y debe tener, al menos, 3 caracteres";
         fName.classList.add("is-invalid");
     } else if (!/^[a-zA-Z]+$/.test(fName.value)) {
         error++;
-        errorName.textContent = "Name must contain only letters";
+        errorName.textContent = "El nombre debe contener sólo letras";
         fName.classList.add("is-invalid");
     } else if (fName.value.length < 3) {
         error++;
-        errorName.textContent = "Name must have, at least, 3 characters";
+        errorName.textContent = "El nombre debe tener, al menos, 3 caracteres";
         fName.classList.add("is-invalid");
     } else {
         errorName.textContent = "";
@@ -134,11 +134,11 @@ function validate() {
     // Email validation
     if (fEmail.value.trim() === "") {
         error++;
-        errorEmail.textContent = "This field is required and must contain an '@' and have, at least, 3 characters";
+        errorEmail.textContent = "Este campo es obligatorio y debe contener una '@' y tener, al menos, 3 caracteres";
         fEmail.classList.add("is-invalid");
     } else if (!/\S+@\S+\.\S+/.test(fEmail.value)) {
         error++;
-        errorEmail.textContent = "Invalid email format";
+        errorEmail.textContent = "Formato de correo electrónico no válido";
         fEmail.classList.add("is-invalid");
     } else {
         errorEmail.textContent = "";
@@ -148,11 +148,11 @@ function validate() {
     // Address validation
     if (fAddress.value.trim() === "") {
         error++;
-        errorAddress.textContent = "This field is required and must have, at least, 3 characters";
+        errorAddress.textContent = "Este campo es obligatorio y debe tener, al menos, 3 caracteres";
         fAddress.classList.add("is-invalid");
     } else if (fAddress.value.length < 3) {
         error++;
-        errorAddress.textContent = "Address must have, at least, 3 characters";
+        errorAddress.textContent = "La dirección debe tener, al menos, 3 caracteres";
         fAddress.classList.add("is-invalid");
     } else {
         errorAddress.textContent = "";
@@ -162,15 +162,15 @@ function validate() {
     // Last Name validation
     if (fLastN.value.trim() === "") {
         error++;
-        errorLastN.textContent = "This field is required and must have, at least, 3 characters";
+        errorLastN.textContent = "Este campo es obligatorio y debe tener, al menos, 3 caracteres";
         fLastN.classList.add("is-invalid");
     } else if (!/^[a-zA-Z]+$/.test(fLastN.value)) {
         error++;
-        errorLastN.textContent = "Last Name must contain only letters";
+        errorLastN.textContent = "El apellido debe contener sólo letras";
         fLastN.classList.add("is-invalid");
     } else if (fLastN.value.length < 3) {
         error++;
-        errorLastN.textContent = "Last Name must have, at least, 3 characters";
+        errorLastN.textContent = "El apellido debe tener, al menos, 3 caracteres";
         fLastN.classList.add("is-invalid");
     } else {
         errorLastN.textContent = "";
@@ -180,15 +180,15 @@ function validate() {
     // Password validation
     if (fPassword.value.trim() === "") {
         error++;
-        errorPassword.textContent = "This field is required";
+        errorPassword.textContent = "Este campo es obligatorio";
         fPassword.classList.add("is-invalid");
     } else if (!/[a-zA-Z]/.test(fPassword.value) || !/\d/.test(fPassword.value)) {
         error++;
-        errorPassword.textContent = "Password must include numbers and letters";
+        errorPassword.textContent = "La contraseña debe incluir números y letras";
         fPassword.classList.add("is-invalid");
     } else if (fPassword.value.length < 4 || fPassword.value.length > 8) {
         error++;
-        errorPassword.textContent = "Password must be between 4 and 8 characters";
+        errorPassword.textContent = "La contraseña debe tener entre 4 y 8 caracteres";
         fPassword.classList.add("is-invalid");
     } else {
         errorPassword.textContent = "";
@@ -198,11 +198,11 @@ function validate() {
     // Phone Number validation
     if (fPhone.value.trim() === "") {
         error++;
-        errorPhone.textContent = "This field is required";
+        errorPhone.textContent = "Este campo es obligatorio";
         fPhone.classList.add("is-invalid");
     } else if (isNaN(fPhone.value) || fPhone.value.length !== 9) {
         error++;
-        errorPhone.textContent = "Invalid phone number!! Must be 9 digits with no letters";
+        errorPhone.textContent = "¡Número de teléfono inválido! Debe tener 9 dígitos sin letras";
         fPhone.classList.add("is-invalid");
     } else {
         errorPhone.textContent = "";
@@ -210,7 +210,7 @@ function validate() {
     }
 
     if (error > 0) {
-        console.log("Please fill the form correctly");
+        console.log("Rellene el formulario correctamente");
     } else {
         console.log("OK");
     }
